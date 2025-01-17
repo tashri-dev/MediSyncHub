81e29772d81ae941d23342c30bf8d83541ba4977
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 02:42:24 +0200

    port and adapter for the services

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/ISlotService.cs
@@ -0,0 +1,7 @@
+﻿namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
+
+public interface ISlotService
+{
+    Task<SlotDto> CreateSlotAsync(DateTime time, Guid doctorId, global::System.String doctorName, global::System.Decimal cost);
+    Task<IEnumerable<SlotDto>> GetAvailableSlotsAsync();
+}
