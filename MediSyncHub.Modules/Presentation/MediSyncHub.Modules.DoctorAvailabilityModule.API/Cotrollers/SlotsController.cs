85f50797058b13e100574475dec96251bebb235a
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:54:37 +0200

    add presentation for the module and wrap the internals in the IoC

---
+++ MediSyncHub.Modules/Presentation/MediSyncHub.Modules.DoctorAvailabilityModule.API/Cotrollers/SlotsController.cs
@@ -0,0 +1,25 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Dtos;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.API.Cotrollers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SlotsController(ISlotService slotService) : ControllerBase
+{
+    [HttpGet()]
+    public async Task<ActionResult<IEnumerable<SlotDto>>> ListAllSlots(CancellationToken cancellationToken = default)
+    {
+        var slots = await slotService.GetAllSlotsAsync(cancellationToken);
+        return Ok(slots);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SlotDto>> CreateSlot(CreateSlotDto request)
+    {
+        var slot = await slotService.CreateSlotAsync(request);
+
+        return CreatedAtAction(nameof(ListAllSlots), slot);
+    }
+}
---
+++ MediSyncHub.Modules/Presentation/MediSyncHub.Modules.DoctorAvailabilityModule.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.API.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAvailabilityModule(this IServiceCollection services, IConfiguration configuration)
+    {
+       services.AddBusinessLayer(configuration);
+
+        return services;
+    }
+}
