0d21c3de8fe181831f87fab3b0c7163c53bb329f
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:27:02 +0200

    implement the repo port

--- MediSyncHub.Modules/DoctorAvailabilityModule/Data/MediSyncHub.Modules.DoctorAvailabilityModule.Data/Repository/ISlotRepository.cs
+++ MediSyncHub.Modules/DoctorAvailabilityModule/Data/MediSyncHub.Modules.DoctorAvailabilityModule.Data/Repository/ISlotRepository.cs
@@ -4,8 +4,9 @@ namespace MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
 
 public interface ISlotRepository
 {
-    Task<IEnumerable<Slot>> GetAvailableSlotsAsync();
-    Task<Slot> GetByIdAsync(Guid id);
-    Task AddAsync(Slot slot);
-    Task UpdateAsync(Slot slot);
+    Task<IEnumerable<Slot>> GetAvailableSlotsAsync(CancellationToken cancellationToken = default); 
+    Task<IEnumerable<Slot>> GetAllSlotsAsync(CancellationToken cancellationToken = default);
+    Task<Slot> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddAsync(Slot slot, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Slot slot, CancellationToken cancellationToken = default);
 }
