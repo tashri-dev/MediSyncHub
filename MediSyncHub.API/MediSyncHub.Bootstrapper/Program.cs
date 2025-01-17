3802f3cedbfeef6e0195b7e99df6d8d556436b43
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:55:22 +0200

    wrap the module in the main bootstrapper

--- MediSyncHub.API/MediSyncHub.Bootstrapper/Program.cs
+++ MediSyncHub.API/MediSyncHub.Bootstrapper/Program.cs
@@ -1,23 +1,56 @@
+using MediSyncHub.Modules.DoctorAvailabilityModule.API.Extensions;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Data;
+using MediSyncHub.SharedKernel.Exetinsions;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = "MediSyncHub API";
+    config.Version = "v1";
+    config.Description = "MediSyncHub API";
+});
+
+// Add shared infrastructure
+builder.Services.AddSharedInfrastructure(builder.Configuration);
+
+// Add modules
+builder.Services.AddAvailabilityModule(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Use NSwag Middlewares
+    app.UseOpenApi();       
+    app.UseSwaggerUi();
 }
 
+// Then the rest of your pipeline
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Run database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var availabilityContext = services.GetRequiredService<DoctorAvailabilityDbContext>();
+        await availabilityContext.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database");
+        throw;
+    }
+}
 
 app.Run();
