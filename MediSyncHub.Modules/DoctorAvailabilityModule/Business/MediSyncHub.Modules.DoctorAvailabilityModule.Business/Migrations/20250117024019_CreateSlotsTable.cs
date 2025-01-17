1edfddaa281e922463a827c346b59c25775b882f
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 04:57:26 +0200

    add migration

---
+++ MediSyncHub.Modules/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Migrations/20250117024019_CreateSlotsTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSlotsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "DoctorAvailability");
+
+            migrationBuilder.CreateTable(
+                name: "Slots",
+                schema: "DoctorAvailability",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsReserved = table.Column<bool>(type: "boolean", nullable: false),
+                    Cost = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Slots",
+                schema: "DoctorAvailability");
+        }
+    }
+}
