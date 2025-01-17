2e2a0c03dd56afad3ae6ff8e02877a8b6a820a50
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:26:02 +0200

    add dbContext

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Data/DoctorAvailabilityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Data
+{
+    internal class DoctorAvailabilityDbContext(DbContextOptions<DoctorAvailabilityDbContext> options)
+        : DbContext(options)
+    {
+        public DbSet<Slot> Slots { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Slot>(builder =>
+            {
+                builder.ToTable("Slots", "DoctorAvailability");
+                builder.HasKey(x => x.Id);
+                builder.Property(x => x.Time).IsRequired();
+                builder.Property(x => x.Cost).HasColumnType("decimal(18,2)");
+            });
+        }
+    }
+}
