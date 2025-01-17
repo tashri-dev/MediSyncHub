1e2a88cba0f5e42c9df80d1941fbc52bdb50a6e1
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 22:56:46 +0200

    remove unneeded method for availability module

--- MediSyncHub.Modules/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/ISlotService.cs
+++ MediSyncHub.Modules/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/ISlotService.cs
@@ -5,7 +5,6 @@ namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
 public interface ISlotService
 {
     Task<SlotDto> CreateSlotAsync(CreateSlotDto slot, CancellationToken cancellationToken = default);
-    Task<IEnumerable<SlotDto>> GetAvailableSlotsAsync(CancellationToken cancellationToken = default);
     Task<SlotDto> GetSlotAsync(Guid id, CancellationToken cancellationToken = default);
     Task<IEnumerable<SlotDto>> GetAllSlotsAsync(CancellationToken cancellationToken = default);
 }
