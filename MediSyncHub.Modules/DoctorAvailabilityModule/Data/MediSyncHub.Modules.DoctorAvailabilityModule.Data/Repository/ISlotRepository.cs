cfcd606e20a7156b2b28b9bc71f7d6a2e413f111
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 02:41:44 +0200

    add data layer with slot model and repository port

---
+++ MediSyncHub.Modules/DoctorAvailabilityModule/Data/MediSyncHub.Modules.DoctorAvailabilityModule.Data/Repository/ISlotRepository.cs
@@ -0,0 +1,11 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Entities;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
+
+public interface ISlotRepository
+{
+    Task<IEnumerable<Slot>> GetAvailableSlotsAsync();
+    Task<Slot> GetByIdAsync(Guid id);
+    Task AddAsync(Slot slot);
+    Task UpdateAsync(Slot slot);
+}
