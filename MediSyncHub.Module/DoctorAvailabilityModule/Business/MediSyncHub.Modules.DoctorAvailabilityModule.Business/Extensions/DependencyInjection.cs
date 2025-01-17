d07680f69f1ba97e441e32f2672c9af200260e0e
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:26:47 +0200

    add dependency injection extension to be used in the IoC

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Extensions/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Data;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Repositories;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDoctorAvailabilityModule(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<DoctorAvailabilityDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+        services.AddScoped<ISlotRepository, SlotRepository>();
+        services.AddScoped<ISlotService, SlotService>();
+        return services;
+    }
+}
