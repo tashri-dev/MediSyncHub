118f3340ae463920b77fbfe9a84a546280e95546
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 23:18:36 +0200

    registered dependencies to IoC

---
+++ MediSyncHub.Modules/AppointmentBookingModule/Core/Application/MediSyncHub.Modules.AppointmentBookingModule.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,31 @@
+using MediSyncHub.Modules.AppointmentBookingModule.Application.IntegrationEvents.Handlers;
+using MediSyncHub.SharedKernel.Events.EventBus;
+using MediSyncHub.SharedKernel.Events.IntegrationEvents.DoctorAvailability;
+using MediSyncHub.SharedKernel.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediSyncHub.Modules.AppointmentBookingModule.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterApplication(this IServiceCollection services)
+    {
+        // Register MediatR handlers
+        services.AddMediatR(cfg => 
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+        // Register event handlers
+        services.AddScoped<SlotCreatedIntegrationEventHandler>();
+        
+        return services;
+    }
+    
+    public static WebApplication ConfigureAppointmentBookingEventBus(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var eventBus = scope.ServiceProvider.GetRequiredService<IEventBus>();
+        eventBus.Subscribe<SlotCreatedIntegrationEvent, SlotCreatedIntegrationEventHandler>();
+        return app;
+    }
+}
