c74268fd6a1715e4cc12e735c1316ddf75f7a13c
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 02:42:09 +0200

    added adapter for repo port

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Repositories/SlotRepository.cs
@@ -0,0 +1,38 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Repositories;
+
+internal class SlotRepository : ISlotRepository
+{
+    private readonly AvailabilityDbContext _context;
+
+    public SlotRepository(AvailabilityDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Slot>> GetAvailableSlotsAsync()
+    {
+        return await _context.Slots
+            .Where(x => !x.IsReserved)
+            .OrderBy(x => x.Time)
+            .ToListAsync();
+    }
+
+    public async Task<Slot> GetByIdAsync(Guid id)
+    {
+        return await _context.Slots.FindAsync(id);
+    }
+
+    public async Task AddAsync(Slot slot)
+    {
+        await _context.Slots.AddAsync(slot);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Slot slot)
+    {
+        _context.Slots.Update(slot);
+        await _context.SaveChangesAsync();
+    }
+}
