0db2c23556858adedfc4ac36f659d7d62e87b03a
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:54:37 +0200

    add presentation for the module and wrap the internals in the IoC

--- MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Extensions/DependencyInjection.cs
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Extensions/DependencyInjection.cs
@@ -3,14 +3,16 @@ using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Repositories;
 using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
 using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDoctorAvailabilityModule(this IServiceCollection services, string connectionString)
+    public static IServiceCollection AddBusinessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DoctorAvailabilityConnectionString");
         services.AddDbContext<DoctorAvailabilityDbContext>(options =>
         {
             options.UseNpgsql(connectionString);
@@ -19,4 +21,5 @@ public static class DependencyInjection
         services.AddScoped<ISlotService, SlotService>();
         return services;
     }
+    
 }
