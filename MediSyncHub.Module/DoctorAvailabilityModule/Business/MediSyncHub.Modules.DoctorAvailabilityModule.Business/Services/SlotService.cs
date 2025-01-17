6829623ed88ae811f025c36db9a69df9cb9e2314
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 02:42:24 +0200

    port and adapter for the services

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/SlotService.cs
@@ -0,0 +1,43 @@
+﻿namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
+
+
+internal class SlotService:ISlotService
+{
+    private readonly ISlotRepository _slotRepository;
+
+    public SlotService(ISlotRepository slotRepository)
+    {
+        _slotRepository = slotRepository;
+    }
+
+    public async Task<IEnumerable<SlotDto>> GetAvailableSlotsAsync()
+    {
+        var slots = await _slotRepository.GetAvailableSlotsAsync();
+        return slots.Select(s => new SlotDto(
+            s.Id,
+            s.Time,
+            s.DoctorId,
+            s.DoctorName,
+            s.IsReserved,
+            s.Cost));
+    }
+
+    public async Task<SlotDto> CreateSlotAsync(
+        DateTime time,
+        Guid doctorId,
+        string doctorName,
+        decimal cost)
+    {
+        var slot = Slot.Create(time, doctorId, doctorName, cost);
+        await _slotRepository.AddAsync(slot);
+
+        return new SlotDto(
+            slot.Id,
+            slot.Time,
+            slot.DoctorId,
+            slot.DoctorName,
+            slot.IsReserved,
+            slot.Cost);
+    }
+}
+
