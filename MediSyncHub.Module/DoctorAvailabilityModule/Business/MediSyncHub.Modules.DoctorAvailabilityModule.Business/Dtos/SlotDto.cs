63fb736d7ae722777f2d740fc076eb3db1bf1bfd
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 03:27:23 +0200

    implement the service port

---
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Dtos/SlotDto.cs
@@ -0,0 +1,38 @@
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Entities;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Dtos;
+
+public record CreateSlotDto
+{
+    public DateTime Time { get; set; }
+    public decimal Cost { get; set; }
+
+    public static implicit operator Slot(CreateSlotDto dto)
+    {
+        return Slot.Create(dto.Time, dto.Cost);
+    }
+
+    public static explicit operator SlotDto(CreateSlotDto dto)
+    {
+        Slot slot = dto;
+        return (SlotDto)slot;
+    }
+}
+
+public class SlotDto
+{
+    public Guid Id { get; set; }
+    public DateTime Time { get; set; }
+    public bool IsReserved;
+    public decimal Cost { get; set; }
+
+    public static implicit operator SlotDto(Slot slot)
+    {
+        return new SlotDto
+        {
+            Id = slot.Id,
+            Time = slot.Time,
+            Cost = slot.Cost
+        };
+    }
+}
--- MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/SlotService.cs
+++ MediSyncHub.Module/DoctorAvailabilityModule/Business/MediSyncHub.Modules.DoctorAvailabilityModule.Business/Services/SlotService.cs
@@ -1,43 +1,47 @@
-﻿namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
+﻿using MediSyncHub.Modules.DoctorAvailabilityModule.Business.Dtos;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Entities;
+using MediSyncHub.Modules.DoctorAvailabilityModule.Data.Repository;
 
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Business.Services;
 
-internal class SlotService:ISlotService
+internal class SlotService(ISlotRepository slotRepository) : ISlotService
 {
-    private readonly ISlotRepository _slotRepository;
-
-    public SlotService(ISlotRepository slotRepository)
-    {
-        _slotRepository = slotRepository;
-    }
-
-    public async Task<IEnumerable<SlotDto>> GetAvailableSlotsAsync()
-    {
-        var slots = await _slotRepository.GetAvailableSlotsAsync();
-        return slots.Select(s => new SlotDto(
-            s.Id,
-            s.Time,
-            s.DoctorId,
-            s.DoctorName,
-            s.IsReserved,
-            s.Cost));
-    }
-
     public async Task<SlotDto> CreateSlotAsync(
         DateTime time,
         Guid doctorId,
         string doctorName,
         decimal cost)
     {
-        var slot = Slot.Create(time, doctorId, doctorName, cost);
-        await _slotRepository.AddAsync(slot);
+        var slot = Slot.Create(time, cost);
+        await slotRepository.AddAsync(slot);
 
-        return new SlotDto(
-            slot.Id,
-            slot.Time,
-            slot.DoctorId,
-            slot.DoctorName,
-            slot.IsReserved,
-            slot.Cost);
+        return slot;
     }
-}
 
+    public async Task<SlotDto> CreateSlotAsync(CreateSlotDto slot, CancellationToken cancellationToken = default)
+    {
+        await slotRepository.AddAsync(slot);
+        return (SlotDto)slot;
+    }
+
+    public async Task<IEnumerable<SlotDto>> GetAvailableSlotsAsync(CancellationToken cancellationToken = default)
+    {
+        var slots = await slotRepository.GetAvailableSlotsAsync(cancellationToken);
+        return slots.Select(slot => (SlotDto)slot);
+    }
+
+    public async Task<SlotDto> GetSlotAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var slot = await slotRepository.GetByIdAsync(id, cancellationToken);
+        return new SlotDto()
+        {
+            Id = slot.Id,
+            IsReserved = slot.IsReserved,
+            Time = slot.Time,
+            Cost = slot.Cost
+        };
+    }
+
+    public async Task GetAllSlotsAsync(CancellationToken cancellationToken = default) =>
+        await slotRepository.GetAllSlotsAsync(cancellationToken);
+}
