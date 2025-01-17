a9972d4adfb465f0286bca48bb36f865ea334712
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 02:41:44 +0200

    add data layer with slot model and repository port

---
+++ MediSyncHub.Modules/DoctorAvailabilityModule/Data/MediSyncHub.Modules.DoctorAvailabilityModule.Data/Entities/Slot.cs
@@ -0,0 +1,31 @@
+﻿using MediSyncHub.SharedKernel.Data;
+
+namespace MediSyncHub.Modules.DoctorAvailabilityModule.Data.Entities;
+
+public class Slot : BaseEntity<Guid>
+{
+    public DateTime Time { get; private set; }
+    public bool IsReserved { get; private set; }
+    public decimal Cost { get; private set; }
+
+    private Slot() { }
+
+    public static Slot Create(
+        DateTime time,
+        decimal cost)
+    {
+        return new Slot
+        {
+            Id = Guid.NewGuid(),
+            Time = time,
+            IsReserved = false,
+            Cost = cost
+        };
+    }
+
+    // This will be called by an integration event handler
+    internal void MarkAsReserved()
+    {
+        IsReserved = true;
+    }
+}
