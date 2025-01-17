19dd6fed9cd56af84b3591dc2880370890282f36
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 23:08:18 +0200

    handled the slotCreatedEvent from Availability module

---
+++ MediSyncHub.Modules/AppointmentBookingModule/Core/Application/MediSyncHub.Modules.AppointmentBookingModule.Application/IntegrationEvents/Handlers/SlotCreatedIntegrationEventHandler.cs
@@ -0,0 +1,40 @@
+using MediSyncHub.Modules.AppointmentBookingModule.Domain.Entities;
+using MediSyncHub.Modules.AppointmentBookingModule.Domain.Repository;
+using MediSyncHub.SharedKernel.Events.IntegrationEvents.DoctorAvailability;
+using MediSyncHub.SharedKernel.Handlers;
+using Microsoft.Extensions.Logging;
+
+namespace MediSyncHub.Modules.AppointmentBookingModule.Application.IntegrationEvents.Handlers;
+
+public class SlotCreatedIntegrationEventHandler(
+    ISlotRepository slotRepository,
+    IUnitOfWork unitOfWork,
+    ILogger<SlotCreatedIntegrationEventHandler> logger)
+    : IIntegrationEventHandler<SlotCreatedIntegrationEvent>
+{
+    public async Task HandleAsync(SlotCreatedIntegrationEvent @event, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var slotReplica = Slot.Create(
+                @event.SlotId,
+                @event.Time,
+                @event.Cost,
+                @event.CreatedAt);
+
+            await slotRepository.AddAsync(slotReplica);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            logger.LogInformation(
+                "Slot replica created for slot {SlotId}",
+                slotReplica.Id);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(
+                e,
+                "Error creating slot replica for slot {SlotId}",
+                @event.SlotId);
+            throw;
+        }
+    }
+}
