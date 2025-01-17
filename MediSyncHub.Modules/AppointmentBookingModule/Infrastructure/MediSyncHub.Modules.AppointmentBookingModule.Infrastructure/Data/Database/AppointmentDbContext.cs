da61e825896c4800317950d763813632308267ba
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-17 23:32:30 +0200

    added BookingDbContext

---
+++ MediSyncHub.Modules/AppointmentBookingModule/Infrastructure/MediSyncHub.Modules.AppointmentBookingModule.Infrastructure/Data/Database/AppointmentDbContext.cs
@@ -0,0 +1,39 @@
+using MediSyncHub.Modules.AppointmentBookingModule.Domain.Entities;
+using MediSyncHub.Modules.AppointmentBookingModule.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSyncHub.Modules.AppointmentBookingModule.Infrastructure.Data.Database;
+
+public class BookingDbContext(DbContextOptions<BookingDbContext> options) : DbContext(options), IUnitOfWork
+{
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Slot> Slots { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Appointment>(builder =>
+        {
+            builder.ToTable("Appointments", "booking");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.SlotId).IsRequired();
+            builder.Property(x => x.PatientId).IsRequired();
+            builder.Property(x => x.PatientName).IsRequired();
+            builder.Property(x => x.ReservedAt).IsRequired();
+            builder.Ignore(x => x.DomainEvents);
+        });
+
+        modelBuilder.Entity<Slot>(builder =>
+        {
+            builder.ToTable("Slots", "booking");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Time).IsRequired();
+            builder.Property(x => x.Cost).HasColumnType("decimal(18,2)");
+        });
+    }
+
+    public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await base.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
