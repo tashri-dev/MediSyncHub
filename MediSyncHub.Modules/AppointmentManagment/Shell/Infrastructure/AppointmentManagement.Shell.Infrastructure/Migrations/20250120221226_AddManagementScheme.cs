d6975c135f21d7187ff458dd959a13f492589287
Taha Ashri <taha.ashri.dv@gmail.com>
2025-01-21 00:55:13 +0200

    add migration

---
+++ MediSyncHub.Modules/AppointmentManagment/Shell/Infrastructure/AppointmentManagement.Shell.Infrastructure/Migrations/20250120221226_AddManagementScheme.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentManagement.Shell.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddManagementScheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "management");
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                schema: "management",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SlotId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientName = table.Column<string>(type: "text", nullable: false),
+                    ReservedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    AppointmentTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments",
+                schema: "management");
+        }
+    }
+}
